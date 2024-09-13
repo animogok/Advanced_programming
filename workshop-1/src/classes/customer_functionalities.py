df7dbffe5ac1b06ad14b7ea7ba6832f1f7fe9696
Sebastian <sebastian.avenda32@gmail.com>
2024-09-13 18:15:50 -0500

    Setting license docstring

--- workshop-1/src/classes/customer_functionalities.py
+++ workshop-1/src/classes/customer_functionalities.py
@@ -1,5 +1,23 @@
 """
 This module works to make the abstract classes of this program
+
+Simple ElectronicDevice Shopping System
+Copyright (C) 13/09/2024  Sebastian Avendaño Rodriguez - sebastian.avenda43@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Source: http://opensource.org/licenses/GPL-3.0
 """
 
 from abc import ABC, abstractmethod
--- workshop-1/src/main.py
+++ workshop-1/src/main.py
@@ -1,3 +1,25 @@
+"""
+This module it's main part of the application were all the code it's executed
+
+Simple ElectronicDevice Shopping System
+Copyright (C) 13/09/2024  Sebastian Avendaño Rodriguez - sebastian.avenda43@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Source: http://opensource.org/licenses/GPL-3.0
+"""
+
 import datetime
 import re
 from classes import customer_functionalities as cf
